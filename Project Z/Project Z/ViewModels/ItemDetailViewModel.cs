9650f22e419ff7e3c7d59ba953d67d25269fd433
austinthale <bbqdpickles@gmail.com>
2019-08-04 21:06:44 -0600

    Add project files.

---
+++ Project Z/Project Z/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Project_Z.Models;
+
+namespace Project_Z.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
